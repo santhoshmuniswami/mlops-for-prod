b79532b21506db4d156256c21ad97dc107c3a7df
Santhosh Muniswami <santhoshpuducherry@gmail.com>
2022-07-17 13:15:06 +0530

    feat: Initial development code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=' This project created to experiment an implement end to end MLOps Solution for ML models in production ',
+    author='Santhosh_M',
+    license='MIT',
+)
